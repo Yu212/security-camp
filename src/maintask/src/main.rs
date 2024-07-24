f8eb7bb204674be53f2ab4e30f4d1f4c3c10a72c
Yu212 <yu212.mc@gmail.com>
2024-07-25 02:27:09 +0900

    [update] 型と演算子を追加

--- src/maintask/src/main.rs
+++ src/maintask/src/main.rs
@@ -1,7 +1,7 @@
-use std::ops;
+use std::ops::{Add, Sub};
 use rand_chacha::ChaChaRng;
 
-const MU: u32 = 1 << 29;
+const MU: Torus32 = 1 << 29;
 const k: usize = 2;
 const N: usize = 512;
 const kN: usize = k * N;
@@ -13,35 +13,39 @@ const basebit: usize = 2;
 fn main() {
 }
 
-fn sample_extract_index(trlwe: TRLWE) -> TLWELv1 {
+fn sample_extract_index(trlwe: &TRLWE) -> TLWELv1 {
     todo!()
 }
 
-fn decomposition(a: usize) -> [i8; l] {
+fn decomposition<const n: usize>(a: Torus32) -> [i8; n] {
     todo!()
 }
 
-fn external_product(c: TRGSW, trlwe: TRLWE) -> TRLWE {
+fn external_product(c: &TRGSW, trlwe: &TRLWE) -> TRLWE {
     todo!()
 }
 
-fn cmux(trgsw: TRGSW, trlwe0: TRLWE, trlwe1: TRLWE) -> TRLWE {
+fn cmux(trgsw: &TRGSW, trlwe0: &TRLWE, trlwe1: &TRLWE) -> TRLWE {
     todo!()
 }
 
-fn blind_rotate(tlwe: TLWELv0, bk: Vec<TRGSW>, test_vec: TRLWE) -> TRLWE {
+fn blind_rotate(tlwe: &TLWELv0, bk: &Vec<TRGSW>, test_vec: &TRLWE) -> TRLWE {
     todo!()
 }
 
-fn gate_bootstrapping_tlwe_to_tlwe(tlwe: TLWELv0, bk: Vec<TRGSW>) -> TLWELv1 {
+fn gate_bootstrapping_tlwe_to_tlwe(tlwe: &TLWELv0, bk: &Vec<TRGSW>) -> TLWELv1 {
     todo!()
 }
 
-fn identity_key_switching(tlwe: TLWELv1, ks: TLWELv0) -> TLWELv0 {
+fn identity_key_switching(tlwe: &TLWELv1, ks: &TLWELv0) -> TLWELv0 {
     todo!()
 }
 
-fn hom_nand(x: TLWELv1, y: TLWELv1) -> TLWELv1 {
+fn hom_nand(x: &TLWELv1, y: &TLWELv1) -> TLWELv1 {
+    todo!()
+}
+
+fn gen_s(rng: &mut ChaChaRng) -> Vec<bool> {
     todo!()
 }
 
@@ -49,50 +53,104 @@ fn encrypt(m: bool, s: &Vec<bool>) -> TLWELv1 {
     todo!()
 }
 
-fn decrypt(tlwe: TLWELv1, s: &Vec<bool>) -> bool {
+fn decrypt(tlwe: &TLWELv1, s: &Vec<bool>) -> bool {
     todo!()
 }
+
+type Torus16 = u16;
+type Torus32 = u32;
+type TorusPoly = [Torus32; N];
 
 type TLWELv0 = TLWE<636>;
 type TLWELv1 = TLWE<kN>;
 struct TLWE<const M: usize> {
-    a: [u16; M],
-    b: u32,
+    a: [Torus16; M],
+    b: Torus16,
 }
-impl<const M: usize> ops::Add<TLWE<M>> for TLWE<M> {
+impl<const M: usize> TLWE<M> {
+    pub fn new(a: [Torus16; M], b: Torus16) -> Self {
+        Self {
+            a,
+            b,
+        }
+    }
+}
+impl<const M: usize> Add<TLWE<M>> for TLWE<M> {
     type Output = TLWE<M>;
 
     fn add(self, rhs: Self) -> Self::Output {
-        todo!()
+        let mut a = [0; M];
+        for i in 0..M {
+            a[i] = self.a[i] + rhs.a[i];
+        }
+        Self::new(a, self.b + rhs.b)
     }
 }
-impl<const M: usize> ops::Sub<TLWE<M>> for TLWE<M> {
+impl<const M: usize> Sub<TLWE<M>> for TLWE<M> {
     type Output = TLWE<M>;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        todo!()
+        let mut a = [0; M];
+        for i in 0..M {
+            a[i] = self.a[i] - rhs.a[i];
+        }
+        Self::new(a, self.b - rhs.b)
     }
 }
 
 struct TRLWE {
-    a0: [u32; N],
-    a1: [u32; N],
-    b: [u32; N],
+    a0: TorusPoly,
+    a1: TorusPoly,
+    b: TorusPoly,
 }
-impl ops::Add<TRLWE> for TRLWE {
-    type Output = TRLWE;
-
-    fn add(self, rhs: Self) -> Self::Output {
+impl TRLWE {
+    pub fn new(a0: TorusPoly, a1: TorusPoly, b: TorusPoly) -> Self {
+        return Self {
+            a0,
+            a1,
+            b,
+        }
+    }
+    pub fn new_zero() -> Self {
         todo!()
     }
 }
-impl ops::Sub<TRLWE> for TRLWE {
+impl Add<TRLWE> for TRLWE {
+    type Output = TRLWE;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut a0 = [0; N];
+        let mut a1 = [0; N];
+        let mut b = [0; N];
+        for i in 0..N {
+            a0[i] = self.a0[i] + rhs.a0[i];
+            a1[i] = self.a1[i] + rhs.a1[i];
+            b[i] = self.b[i] + rhs.b[i];
+        }
+        Self::new(a0, a1, b)
+    }
+}
+impl Sub<TRLWE> for TRLWE {
     type Output = TRLWE;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        todo!()
+        let mut a0 = [0; N];
+        let mut a1 = [0; N];
+        let mut b = [0; N];
+        for i in 0..N {
+            a0[i] = self.a0[i] - rhs.a0[i];
+            a1[i] = self.a1[i] - rhs.a1[i];
+            b[i] = self.b[i] - rhs.b[i];
+        }
+        Self::new(a0, a1, b)
     }
 }
 
 struct TRGSW {
+    mat: [[TorusPoly; 3]; l * 3],
+}
+impl TRGSW {
+    pub fn new(mu: Torus32) -> Self {
+        todo!()
+    }
 }
